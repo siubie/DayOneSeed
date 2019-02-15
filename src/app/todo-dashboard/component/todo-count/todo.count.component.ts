a4121501865ac0e0fc2bcd407b1f0a79b36b18c7
Putra Prima Arhandi <putraprima@gmail.com>
2019-02-15 07:41:31 +0700

    finished

--- src/app/todo-dashboard/component/todo-count/todo.count.component.ts
+++ src/app/todo-dashboard/component/todo-count/todo.count.component.ts
@@ -1,7 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Todo } from '../../models/todo.model';
+import { TodoDashboardComponent } from '../../containers/todo.dashboard.component';
 @Component({
   selector: 'todo-count',
   styleUrls: [],
   templateUrl: 'todo.count.html'
 })
-export class TodoCountComponent {}
+export class TodoCountComponent {
+  @Input()
+  todos: Todo[];
+  constructor() {}
+  counterFinished(): number {
+    if (this.todos) {
+      return this.todos.filter((todo: Todo) => todo.status).length;
+    } else {
+      return 0;
+    }
+  }
+}
